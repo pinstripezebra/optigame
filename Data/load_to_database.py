3220767ec010cff64460e77a24f3843c80f494cc
pinstripezebra <seelcs12@gmail.com>
2025-03-29 16:32:52 -0700

    adding function for addign data to games table

--- Data/load_to_database.py
+++ Data/load_to_database.py
@@ -22,13 +22,33 @@ def create_games_table():
     cursor.execute("""CREATE TABLE IF NOT EXISTS products (
         asin varchar(255),
         title varchar(255),
-        price varchar(255),
+        price varchar(1000),
         rating varchar(255),
         rating_count varchar(255),
         description text,
         image_url varchar(255),
         product_url varchar(255)
     )""")
+
+    conn.commit()
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+def populate_games_table(df):
+
+    """ Connect to the PostgreSQL database and populate games table
+    with data from the dataframe."""
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    # Iterate over the rows of the DataFrame and insert each row into the table
+    for index, row in df.iterrows():
+        cursor.execute("""INSERT INTO products (asin, title, price, rating, rating_count, description, image_url, product_url)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+                       (row['asin'], row['title'], row['price'], row['rating'], row['reviews_count'],
+                        row['description'], row['image_url'], row['product_url']))
 
     conn.commit()
     # Close the cursor and connection
